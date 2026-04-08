ee212a659a9d159f08d81cbc4b0e4e9d75eeb481
ninayal <ninayal198@gmail.com>
2026-04-08 16:48:36 +0700

    nina

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@bp.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@bp.route('/page3')
+def page3():
+    return render_template('page3.html')
